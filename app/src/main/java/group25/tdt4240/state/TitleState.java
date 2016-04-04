a3d82dbc66dd2df78e0d33abff21cea8bbfd5b3e
Dag Erik Tangvik <daget@stud.ntnu.no>
2016-04-04 14:39:21 +0200

    TitleState
    Fix

--- app/src/main/java/group25/tdt4240/state/TitleState.java
+++ app/src/main/java/group25/tdt4240/state/TitleState.java
@@ -1,6 +1,7 @@
 package group25.tdt4240.state;
 
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 import group25.tdt4240.entity.Button;
 import sheep.graphics.Image;
 import sheep.game.State;
@@ -9,7 +10,7 @@ import sheep.input.TouchListener;
 /**
  * Created by Meneth on 2016-03-31.
  */
-public class TitleState extends State implements TouchListener{
+public class TitleState extends SuperState implements TouchListener{
     private Image playButtonImage = new Image();
     private Image settingsButtonImage = new Image();
     private Image aboutButtonImage = new Image();
@@ -17,10 +18,11 @@ public class TitleState extends State implements TouchListener{
     Button settingsButton = new Button(settingsButtonImage);
     Button aboutButton = new Button(aboutButtonImage);
 
+
     public TitleState(){
-        playButton.setPosition(1,1);
-        settingsButton.setPosition(1,1);
-        aboutButton.setPosition(1,1);
+        playButton.setPosition(1, 1);
+        settingsButton.setPosition(1, 1);
+        aboutButton.setPosition(1, 1);
     }
 
     public void draw(Canvas canvas){
@@ -29,7 +31,27 @@ public class TitleState extends State implements TouchListener{
         aboutButton.draw(canvas);
     }
 
-    public void update(float dt){
+    public boolean onTouchUp(MotionEvent event){
+        float clickY = event.getY();
+        float clickX = event.getX();
+        if (playButton.getBoundingBox().contains(clickX, clickY)){
+            getGame().popState();
+            getGame().pushState(new PlayState());
+            return true;
+        }
+        if (settingsButton.getBoundingBox().contains(clickX, clickY)){
+            getGame().popState();
+            getGame().pushState(new SettingsState());
+            return true;
+        }
+        if (aboutButton.getBoundingBox().contains(clickX, clickY)){
+            getGame().popState();
+            getGame().pushState(new TitleState());
+            return true;
+        }
+        return false;
+    }
 
+    public void update(float dt){
     }
 }

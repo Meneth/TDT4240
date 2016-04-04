d48194e0f3345e911c7baf9d9e65cce900a9f895
unknown <omsaggau@gmail.com>
2016-04-04 12:51:29 +0200

    Beggining work on tile classes and created Constants class

---
+++ app/src/main/java/group25/tdt4240/Constants.java
@@ -0,0 +1,10 @@
+package group25.tdt4240;
+
+/**
+ * Created by Ole on 04/04/2016.
+ */
+public class Constants {
+
+    public static float TILE_WIDTH = 30;
+    public static float TILE_HEIGHT = 30;
+}
--- app/src/main/java/group25/tdt4240/entity/tile/BuildTile.java
+++ app/src/main/java/group25/tdt4240/entity/tile/BuildTile.java
@@ -1,7 +1,28 @@
 package group25.tdt4240.entity.tile;
 
+import group25.tdt4240.entity.tower.Tower;
+import sheep.graphics.Image;
+
 /**
  * Created by Meneth on 2016-03-31.
  */
 public class BuildTile extends Tile {
+    private Tower tower;
+
+    public BuildTile(Image image){
+        super(image);
+    }
+
+    public Tower getTower() {
+        return tower;
+    }
+    public void setTower(Tower tower) {
+        this.tower = tower;
+    }
+    public boolean isBuilt(){
+        return (tower != null);
+    }
+
+
+
 }

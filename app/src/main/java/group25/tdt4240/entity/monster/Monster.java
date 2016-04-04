81d83a11e3382b723c761e2be8ca8242a08e5065
Meneth <github@meneth.com>
2016-04-04 13:42:35 +0200

    Image constructors

--- app/src/main/java/group25/tdt4240/entity/monster/Monster.java
+++ app/src/main/java/group25/tdt4240/entity/monster/Monster.java
@@ -1,9 +1,13 @@
 package group25.tdt4240.entity.monster;
 
 import group25.tdt4240.entity.MovableEntity;
+import sheep.graphics.Image;
 
 /**
  * Created by Meneth on 2016-03-31.
  */
 public abstract class Monster extends MovableEntity {
+    public Monster(Image image) {
+        super(image);
+    }
 }

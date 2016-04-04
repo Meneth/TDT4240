bb7bc5db11c825389c08bb66e16dc26686248f96
Meneth <github@meneth.com>
2016-04-04 14:28:00 +0200

    Clickable interface

--- app/src/main/java/group25/tdt4240/entity/Button.java
+++ app/src/main/java/group25/tdt4240/entity/Button.java
@@ -1,16 +1,27 @@
 package group25.tdt4240.entity;
 
 import android.graphics.Canvas;
+import android.view.MotionEvent;
 
+import sheep.graphics.Image;
 import sheep.gui.Widget;
 
 /**
  * Created by Meneth on 2016-03-31.
  */
-public class Button extends Widget {
+public class Button extends Entity implements Clickable {
+    public Button(Image image) {
+        super(image);
+    }
 
     @Override
     public void draw(Canvas canvas) {
         // TODO - Write draw
+    }
+
+    @Override
+    public boolean onTouchDown(MotionEvent event) {
+        // TODO
+        return false;
     }
 }
--- app/src/main/java/group25/tdt4240/entity/tile/BuildTile.java
+++ app/src/main/java/group25/tdt4240/entity/tile/BuildTile.java
@@ -1,12 +1,15 @@
 package group25.tdt4240.entity.tile;
 
+import android.view.MotionEvent;
+
+import group25.tdt4240.entity.Clickable;
 import group25.tdt4240.entity.tower.Tower;
 import sheep.graphics.Image;
 
 /**
  * Created by Meneth on 2016-03-31.
  */
-public class BuildTile extends Tile {
+public class BuildTile extends Tile implements Clickable {
     private Tower tower;
 
     public BuildTile(Image image){
@@ -24,5 +27,9 @@ public class BuildTile extends Tile {
     }
 
 
-
+    @Override
+    public boolean onTouchDown(MotionEvent event) {
+        // TODO
+        return false;
+    }
 }

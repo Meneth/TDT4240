a59e636de4b7fcd36bf40cfc443a4ff25f90a301
Meneth <github@meneth.com>
2016-04-04 14:03:26 +0200

    Button as a widget

--- app/src/main/java/group25/tdt4240/entity/Button.java
+++ app/src/main/java/group25/tdt4240/entity/Button.java
@@ -1,7 +1,16 @@
 package group25.tdt4240.entity;
 
+import android.graphics.Canvas;
+
+import sheep.gui.Widget;
+
 /**
  * Created by Meneth on 2016-03-31.
  */
-public class Button extends Entity {
+public class Button extends Widget {
+
+    @Override
+    public void draw(Canvas canvas) {
+        // TODO - Write draw
+    }
 }

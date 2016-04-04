15c23fecae11edfc87116fd93656ccb4fcebb963
Meneth <github@meneth.com>
2016-04-04 13:32:16 +0200

    Basic entity logic

--- app/src/main/java/group25/tdt4240/entity/Entity.java
+++ app/src/main/java/group25/tdt4240/entity/Entity.java
@@ -1,6 +1,9 @@
 package group25.tdt4240.entity;
 
+import android.graphics.Canvas;
+
 import sheep.game.Sprite;
+import sheep.graphics.Image;
 
 /**
  * Created by Meneth on 2016-03-31.
@@ -8,5 +11,22 @@ import sheep.game.Sprite;
 
 
 public abstract class Entity extends Sprite {
+    private final Image image;
 
+    public Entity(Image image) {
+        super(image);
+        this.image = image;
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        // TODO - Possibly add something here
+    }
+
+    @Override
+    public void update(float dt) {
+        super.update(dt);
+        // TODO - Update logic?
+    }
 }

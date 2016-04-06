ee1c28fbc831af7fea20cd3def3ace877088af50
Dag Erik Tangvik <daget@stud.ntnu.no>
2016-04-06 12:51:06 +0200

    Settings button added
    Added settings menu button

--- app/src/main/java/group25/tdt4240/state/SettingsState.java
+++ app/src/main/java/group25/tdt4240/state/SettingsState.java
@@ -1,9 +1,41 @@
 package group25.tdt4240.state;
 
-import sheep.game.State;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import group25.tdt4240.entity.Button;
+import sheep.graphics.Image;
 
 /**
  * Created by Meneth on 2016-03-31.
  */
 public class SettingsState extends SuperState {
+    private Image returnButtonImage = new Image();
+    Button returnButton = new Button(returnButtonImage);
+
+    public SettingsState(){
+        returnButton.setPosition(1, 1);
+        addEntities(returnButton);
+    }
+
+    // Add toggle sound-functionality
+
+    public boolean onTouchUp(MotionEvent event){
+        float clickY = event.getY();
+        float clickX = event.getX();
+        if (returnButton.getBoundingBox().contains(clickX, clickY)){
+            getGame().popState();
+            getGame().pushState(new TitleState());
+            return true;
+        }
+        return false;
+    }
+
+    public void update(float dt){
+        super.update(dt);
+    }
+
+    public void draw(Canvas canvas) {
+        //Draw buttons
+        playButton.draw(canvas);
+    }
 }

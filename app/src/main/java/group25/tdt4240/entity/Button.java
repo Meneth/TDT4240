1f63d649935b8cc8713f0efd9eef52f8c80e749e
Meneth <github@meneth.com>
2016-04-06 18:08:16 +0200

    Buttom probably doesn't need own draw method

--- app/src/main/java/group25/tdt4240/entity/Button.java
+++ app/src/main/java/group25/tdt4240/entity/Button.java
@@ -12,13 +12,6 @@ public class Button extends Entity implements Clickable {
         super(image);
     }
 
-    /**
-     * @param canvas Current canvas
-     * @Override public void draw(Canvas canvas) {
-     * // TODO - Write draw
-     * }
-     */
-
     @Override
     public boolean onTouchDown(MotionEvent event) {
         // TODO

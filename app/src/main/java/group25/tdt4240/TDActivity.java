bcdbb02661fd543bb48ef4862161502637f70dc7
Meneth <github@meneth.com>
2016-04-10 13:40:22 +0200

    Hid the top bar

--- app/src/main/java/group25/tdt4240/TDActivity.java
+++ app/src/main/java/group25/tdt4240/TDActivity.java
@@ -1,10 +1,14 @@
 package group25.tdt4240;
 
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
 
 import group25.tdt4240.state.TitleState;
 import sheep.game.*;
@@ -22,5 +26,19 @@ public class TDActivity extends Activity {
         game.pushState(new TitleState());
         // View the game.
         setContentView(game);
+
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            // Remember that you should never show the action bar if the
+            // status bar is hidden, so hide that too if necessary.
+            ActionBar actionBar = getActionBar();
+            //actionBar.hide();
+        }
     }
 }

fb8790e7f342d744cecf3fd1d57334c4b70e5b0a
Meneth <github@meneth.com>
2016-04-11 13:10:30 +0200

    Path-following logic for monsters

--- app/src/main/java/group25/tdt4240/entity/MovableEntity.java
+++ app/src/main/java/group25/tdt4240/entity/MovableEntity.java
@@ -1,16 +1,27 @@
 package group25.tdt4240.entity;
 
 import sheep.graphics.Image;
+import sheep.math.Vector2;
 
 /**
  * Created by Meneth on 2016-03-31.
  */
 public abstract class MovableEntity extends Entity {
-
     /**
      * @param image The image the sprite is to be generated from
      */
     public MovableEntity(Image image) {
         super(image);
+    }
+
+    public void setTarget(Vector2 target) {
+        float speed = getSpeed().getLength();
+        float dx = getX() - target.getX();
+        float dy = getY() - target.getY();
+
+        Vector2 v = new Vector2(dx, dy);
+        float speedScale = speed / v.getLength();
+        v.multiply(speedScale);
+        setSpeed(v);
     }
 }

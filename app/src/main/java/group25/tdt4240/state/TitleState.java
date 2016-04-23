891817d500faef3f00896c4153ef20343295ee71
Meneth <github@meneth.com>
2016-04-23 13:57:34 +0200

    Added game name to TitleState

--- app/src/main/java/group25/tdt4240/state/TitleState.java
+++ app/src/main/java/group25/tdt4240/state/TitleState.java
@@ -1,7 +1,10 @@
 package group25.tdt4240.state;
 
+import group25.tdt4240.R;
+import group25.tdt4240.entity.Entity;
 import group25.tdt4240.utility.Constants;
 import group25.tdt4240.entity.button.*;
+import sheep.graphics.Image;
 import sheep.input.TouchListener;
 
 /**
@@ -9,12 +12,21 @@ import sheep.input.TouchListener;
  */
 public class TitleState extends SuperState implements TouchListener {
     public TitleState() {
+        Entity title = new Entity(new Image(R.drawable.title)) {
+            @Override
+            public int getPriority() {
+                return 0;
+            }
+        };
+        title.setScale(0.5f, 0.5f);
+        title.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 1);
         Button playButton = new PlayButton();
-        playButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 2);
+        playButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 2.5f);
         Button settingsButton = new SettingsButton();
-        settingsButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 4);
+        settingsButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 4.5f);
         Button aboutButton = new AboutButton();
-        aboutButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 6);
-        addEntities(playButton, settingsButton, aboutButton);
+        aboutButton.setPosition(Constants.SCREEN_WIDTH / 2, (Constants.SCREEN_HEIGHT / 8) * 6.5f);
+        addEntities(title, playButton, settingsButton, aboutButton);
+
     }
 }
